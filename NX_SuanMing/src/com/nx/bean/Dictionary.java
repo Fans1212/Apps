ec03a486fd43dce6854f0b4448f2e9579e02ae32
devuser01 <LYNSH@LYNSH-PC>
2013-09-21 22:18:14 +0800

    注意用第一个按钮。  Commit and Push

--- NX_SuanMing/src/com/nx/bean/Dictionary.java
+++ NX_SuanMing/src/com/nx/bean/Dictionary.java
@@ -2,9 +2,12 @@ package com.nx.bean;
 /**
  * 
  * @author 吴正兵
- *
+ *c
  */
 public class Dictionary {
 
-	// For test    
+	// For test 
+	
+	// 先修改 ,但是不提交， Team->Pull
+    // Pull好了以后，如果没有冲突就 Team->Commit&Push
 }
